c3520633a5c65bcfd0aebbd44b3c3a922899d935
surfacepro8please <108337938+surfacepro8please@users.noreply.github.com>
2023-10-31 11:50:22 +0900

    時刻とプルダウンをコンポーネント化

--- src/components/SimulatorRegi.vue
+++ src/components/SimulatorRegi.vue
@@ -4,28 +4,9 @@
         <div class="bg-red-100">
             <div class="flex items-center ml-8">
                 <div class="flex flex-col mr-4 ">
-                    <div class="date w-13 h-5 text-center text-black font-bold font-['Inter'] mb-1 mr-4">{{ formattedDate }}
-                    </div>
-                    <div class="time w-15 h-6 text-center text-black text-2xl font-bold font-['Inter'] mr-4">{{
-                        formattedTime }}</div>
+                    <TimeDisplay/>
                 </div>
-                <div class="relative">
-                    <button @click="toggleDropdown"
-                        class="bg-white border text-3xl border-gray-500 text-green-700 font-bold py-2 px-7 rounded ">
-                        登録
-                    </button>
-                    <!-- Dropdown -->
-                    <div v-if="showDropdown" class="absolute top-fill w-32">
-                        <button
-                            class="bg-white border text-2xl border-gray-500 text-green-700 font-bold py-2 px-3.5 rounded whitespace-no-wrap">
-                            フライヤー
-                        </button>
-                        <button
-                            class="bg-white border text-3xl border-gray-500 text-green-700 font-bold py-2 px-7 rounded">
-                            休止
-                        </button>
-                    </div>
-                </div>
+                    <RegiPulldown/>
                 <span class="w-10 h-5 text-black font-bold self-start">demo</span>
             </div>
         </div>
@@ -34,34 +15,25 @@
     </div>
 </template>
 <script>
-import moment from "moment"
+import TimeDisplay from "../components/parts/TimeDisplay.vue"
+import RegiPulldown from "../components/parts/RegiPulldown.vue"
 export default {
-    data() {
-        return {
-            currentDateTime: moment(),
-            showDropdown: false
-        }
+    components:{
+        TimeDisplay,
+        RegiPulldown
     },
-    methods: {
-        toggleDropdown() {
-            this.showDropdown = !this.showDropdown;
-        }
-    },
-    computed: {
-        formattedDate() {
-            return this.currentDateTime.format('MM/DD');
-        },
-        formattedTime() {
-            return this.currentDateTime.format('HH:mm');
-        }
-    },
-    mounted() {
-        this.interval = setInterval(() => {
-            this.currentDateTime = moment();
-        }, 1000);
-    },
-    beforeUnmount() {
-        clearInterval(this.interval);
-    }
+    // data() {
+    //     return {
+    //         showDropdown: false
+    //     }
+    // },
+    // methods: {
+    //     toggleDropdown() {
+    //         this.showDropdown = !this.showDropdown;
+    //     }
+    // },
+    // beforeUnmount() {
+    //     clearInterval(this.interval);
+    // }
 }
 </script>
---
+++ src/components/parts/RegiPulldown.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="relative">
+        <button @click="toggleDropdown"
+            class="bg-white border text-3xl border-gray-500 text-green-700 font-bold py-2 px-7 rounded ">
+            登録
+        </button>
+        <!-- Dropdown -->
+        <div v-if="showDropdown" class="absolute top-fill w-32">
+            <button
+                class="bg-white border text-2xl border-gray-500 text-green-700 font-bold py-2 px-3.5 rounded whitespace-no-wrap">
+                フライヤー
+            </button>
+            <button class="bg-white border text-3xl border-gray-500 text-green-700 font-bold py-2 px-7 rounded">
+                休止
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showDropdown: false
+        }
+    },
+    methods: {
+        toggleDropdown() {
+            this.showDropdown = !this.showDropdown;
+        }
+    },
+    beforeUnmount() {
+        clearInterval(this.interval);
+    }
+
+}
+
+</script>
