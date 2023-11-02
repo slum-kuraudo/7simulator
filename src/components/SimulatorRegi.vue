f8d6d7992b9000547ac64a3ee2739b793674454e
surfacepro8please <108337938+surfacepro8please@users.noreply.github.com>
2023-11-02 12:00:22 +0900

    レジ画面一応完成

--- src/components/SimulatorRegi.vue
+++ src/components/SimulatorRegi.vue
@@ -19,10 +19,79 @@
                     <RegiTotal />
                 </table>
             </div>
-            <!-- <img src="../assets/WindowsXP.jpg" alt="Your Image Description" class="absolute left-0 bottom-0 max-w-xs max-h-xs"> -->
+            <div class="flex">
+                <RegiFlyerButton />
+            </div>
+            <div class="flex item-center">
+                <div class="rounded">
+                    <div class="flex flex-wrap w-64 p-4 bg-gray-200 rounded">
+                        <RegiContorllet />
+                        <!-- <img src="../assets/WindowsXP.jpg" alt="Your Image Description" class="absolute left-0 bottom-0 max-w-xs max-h-xs"> -->
+                    </div>
+                </div>
+                <div class="rounded">
+                    <div class="flex flex-wrap w-48 h-90 p-4 bg-gray-500">
+                        <button class="w-16 h-16 p-4 m-1 bg-green-500 text-white rounded hover:bg-blue-600 col-span-2">7co</button>
+                        <button
+                            class="w-16 h-16 p-2 m-1 bg-green-500 text-white rounded hover:bg-blue-600 text-xs whitespace-nowrap col-span-2">電子マネー</button>
+                        <button
+                            class="w-16 h-16 p-4 m-1 bg-white text-blue-700 rounded hover:bg-blue-600 font-bold text-3xl col-span-2">9</button>
+                        <button
+                            class="w-16 h-16 p-4 m-1 bg-white text-red-700 rounded hover:bg-blue-600 font-bold text-3xl col-span-2">9</button>
+
+                        <button
+                            class="w-16 h-16 p-4 m-1 bg-white text-blue-700 rounded hover:bg-blue-600 font-bold text-3xl col-span-2">19</button>
+                        <button
+                            class="w-16 h-16 p-4 m-1 bg-white text-red-700 rounded hover:bg-blue-600 font-bold text-3xl col-span-2">19</button>
+
+                        <button
+                            class="w-16 h-16 p-4 m-1 bg-white text-blue-700 rounded hover:bg-blue-600 font-bold text-3xl col-span-2">29</button>
+                        <button
+                            class="w-16 h-16 p-4 m-1 bg-white text-red-700 rounded hover:bg-blue-600 font-bold text-3xl col-span-2">29</button>
+
+                        <button
+                            class="w-16 h-16 p-4 m-1 bg-white text-blue-700 rounded hover:bg-blue-600 font-bold text-3xl col-span-2">49</button>
+                        <button
+                            class="w-16 h-16 p-4 m-1 bg-white text-red-700 rounded hover:bg-blue-600 font-bold text-3xl col-span-2">49</button>
+
+                        <button
+                            class="w-16 h-16 p-4 m-1 bg-white text-blue-700 rounded hover:bg-blue-600 font-bold text-3xl col-span-2">50</button>
+                        <button
+                            class="w-16 h-16 p-4 m-1 bg-white text-red-700 rounded hover:bg-blue-600 font-bold text-3xl col-span-2">50</button>
+
+                        <button class="w-32 h-16 m-1 bg-blue-500 text-white rounded hover:bg-blue-600 col-span-2 ">登録/リピート</button>
+                    </div>
+                </div>
+                <div class="rounded">
+                    <div class="flex flex-wrap w-80 h-40 py-56 bg-gray-900">
+                        <button
+                            class="w-32 h-16 m-1 bg-green-500 text-white rounded hover:bg-blue-600 col-span-2">7co</button>
+                        <button
+                            class="w-32 h-16 m-1 bg-green-500 text-white rounded hover:bg-blue-600 col-span-2">インターネット</button>
+                        <button
+                            class="w-32 h-16 m-1 bg-green-500 text-white rounded hover:bg-blue-600 col-span-2">電子マネー</button>
+                        <button
+                            class="w-32 h-16 m-1 bg-green-500 text-white rounded hover:bg-blue-600 col-span-2">チケット</button>
+                        <button
+                            class="w-16 h-16 m-1 bg-green-500 text-white rounded hover:bg-blue-600 col-span-2">領収書</button>
+                        <button
+                            class="w-16 h-16 m-1 bg-green-500 text-white rounded hover:bg-blue-600 col-span-2">CG</button>
+                        <button
+                            class="w-32 h-16 m-1 bg-green-500 text-white rounded hover:bg-blue-600 col-span-2">返品</button>
+                        <button
+                            class="w-16 h-16 m-1 bg-green-500 text-white rounded hover:bg-blue-600 col-span-2">責任者</button>
+                        <button
+                            class="w-16 h-16 m-1 bg-green-500 text-white rounded hover:bg-blue-600 col-span-2">キャンセル</button>
+                        <button
+                            class="w-16 h-16 m-1 bg-green-500 text-white rounded hover:bg-blue-600 col-span-2">情報</button>
+                        <button
+                            class="w-16 h-16 m-1 bg-green-500 text-white rounded hover:bg-blue-600 col-span-2">呼出</button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="bg-orange-100">3</div>
-        <div class="bg-amber-100">4</div>
+        <!-- <div class="bg-orange-100">3</div>
+        <div class="bg-amber-100">4</div> -->
     </div>
 </template>
 <script>
@@ -30,12 +99,16 @@ import TimeDisplay from "./parts/TimeDisplay.vue"
 import RegiPulldown from "./parts/RegiPulldown.vue"
 import RegiProduct from "./parts/RegiProduct.vue";
 import RegiTotal from "./parts/RegiTotal.vue";
+import RegiFlyerButton from "./parts/RegiFlyerButton.vue";
+import RegiContorllet from "./parts/RegiContorllet.vue";
 export default {
     components: {
         TimeDisplay,
         RegiPulldown,
         RegiProduct,
-        RegiTotal
+        RegiTotal,
+        RegiFlyerButton,
+        RegiContorllet
     }
 }
 </script>
---
+++ src/components/parts/RegiFlyerButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <button v-for="column in columns" :key="column.label"
+        class="w-full sm:w-1/8 bg-gray-300 hover:bg-gray-700 text-black font-bold py-1 px-4 rounded">{{ column.label
+        }}</button>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            columns: [
+                { label: 'レジ袋' },
+                { label: 'フライヤー' },
+                { label: '中華まん' },
+                { label: 'コーヒー' },
+                { label: '雑誌' },
+                { label: 'おでん' },
+                { label: 'empty' },
+                { label: 'empty' }
+            ]
+        }
+    }
+
+}
+</script>
